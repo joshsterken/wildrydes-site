c3e56a9dbb19cd5b819ad61c9c32cbe1ed584879
joshsterken <54761154+joshsterken@users.noreply.github.com>
2023-11-19 21:31:45 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        userPoolId:         'us-west-2_Gi84X6WEm',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '5qr92hmbu7ls8ooepbtj2i1lnb',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             'us-west-2'          // e.g. us-east-2
     },
     api: {
         invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
